4b331c82497a7ffa7bde1d0dfb838cee1489a564
STRIKERZERO\Striker Zero <poom9119@hotmail.com>
2021-01-29 20:34:26 +0700

    Send Assignment Week1

---
+++ 1610701128/Assets/WordS.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordS : MonoBehaviour
+{
+    public string search;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void Search()
+    {
+        string[] word = { "GGEZ", "NiceGame", "Booooom", "Go", "Move" };
+        string inp = inputField.GetComponent<InputField>().text;
+        bool checker = false;
+        foreach (string word1 in word)
+        {
+            if (word1.Equals(inp))
+            {
+                checker = true;
+                break;
+            }
+        }
+        if (checker)
+        {
+            textDisplay.GetComponent<Text>().text = "Found";
+        }
+        else
+        {
+            textDisplay.GetComponent<Text>().text = "Not Found";
+        }
+    }
+
+
+}
